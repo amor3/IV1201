8072a7fa332e0635e339b23bbe79b28e6bc015b6
akhlad1 <akhlad@kth.se>
2015-02-20 18:11:46 +0100

    Fixes #24, did internationalization with some simple javadoc in two classes just for fun

--- src/main/java/se/kth/integration/CompetenceDAO.java
+++ src/main/java/se/kth/integration/CompetenceDAO.java
@@ -25,6 +25,12 @@ public class CompetenceDAO {
     @PersistenceContext(unitName = "RDB_PU")
     private EntityManager em;
 
+    /**
+     * create competences in both languages
+     * @param nameEn Competence Name in English
+     * @param nameSV Competence name in Swedish
+     * 
+     */
     public void creatCompetenc(String nameEN, String nameSV) {
         if (nameEN != null && nameSV != null) {
             CompetenceSv sv= null;
@@ -46,7 +52,12 @@ public class CompetenceDAO {
 
         }
     }
-
+/**
+     *
+     * This Method for removing a competence
+     * @param name name of the competence
+     * 
+     */
     public void removeCompetence(String name) {
 
     }
