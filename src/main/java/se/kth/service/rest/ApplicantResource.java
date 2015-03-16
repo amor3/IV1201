c82b9499dfad4deca14177c327e5649fd53e9c1b
More <amore@kth.se>
2015-03-16 13:35:37 +0100

    Fixes #54, Added full rest api for applicant handling.

--- src/main/java/se/kth/service/rest/ApplicantResource.java
+++ src/main/java/se/kth/service/rest/ApplicantResource.java
@@ -1,29 +1,25 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package se.kth.service.rest;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.PathParam;
-import javax.ws.rs.Consumes;
 import javax.ws.rs.Produces;
 import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PUT;
-import javax.json.Json;
-import javax.json.JsonObject;
-import javax.ws.rs.DELETE;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import se.kth.integration.DoesNotExistException;
+import se.kth.integration.DuplicateEntryException;
 import se.kth.integration.NullArgumentException;
 import se.kth.integration.PersonDAO;
 import se.kth.integration.PersonDTO;
@@ -40,30 +36,24 @@ public class ApplicantResource {
 
     @Context
     private UriInfo context;
-    
+
     @EJB
     private PersonDAO personDAO;
 
     public ApplicantResource() {
     }
-    
-    /*
-    
-        System.out.println("email" + this.email);
-        System.out.println("pass" + this.password);
-        System.out.println("name" + this.firstname);
-        System.out.println("name2" + this.surname);
-        System.out.println("ssn" + this.ssn);
-        System.out.println("from" + this.availableFrom);
-        System.out.println("to" + this.availableTo);
-        System.out.println(droppableCManager.getDroppedCompetences().get(0));
-    */
 
+    /**
+     * getApplicants lets you retrieve a applicant by just passing a email
+     *
+     * @param email searchable on the email
+     * @return a person
+     */
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     @Path("/{email}")
-    public Response getApplicant(@PathParam("email") String email) {        
-        
+    public Response getApplicant(@PathParam("email") String email) {
+
         PersonInterface person = null;
         try {
             person = personDAO.getPerson(email);
@@ -72,48 +62,99 @@ public class ApplicantResource {
         } catch (NullArgumentException ex) {
             Logger.getLogger(ApplicantResource.class.getName()).log(Level.SEVERE, null, ex);
         }
-         
-        return  Response.status(200).entity(person).build();
+
+        return Response.status(200).entity(person).build();
     }
 
-    
-    /*
+    /**
+     * createApplicant lets you create a new applicant by sending json
+     *
+     * @param email
+     * @param firstname
+     * @param surname
+     * @param ssn
+     * @param personDTO
+     * @return
+     */
     @POST
-    @Path("/createuser")
-    @Consumes(MediaType.APPLICATION_JSON)
-    public Response createUser(User user) {
-        JsonObject value;
-// Check if the user exist already
-        User addThisUser = em.find(User.class, user.getUsername());
-        if (addThisUser == null) {
-            em.persist(user);
+    @Path("/create/{email}/{firstname}/{surname}/{ssn}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response createApplicant(@PathParam("email") String email,
+            @PathParam("firstname") String firstname,
+            @PathParam("surname") String surname,
+            @PathParam("ssn") String ssn) {
+        JsonObject value = null;
+
+        PersonDTO personDTO = new PersonDTO();
+        personDTO.setEmail(email);
+        personDTO.setFirstname(firstname);
+        personDTO.setSurname(surname);
+        personDTO.setSsn(ssn);
+        personDTO.setPassword("Standard");
+
+        // Check if the user exist already
+        PersonInterface addApplicant;
+        try {
+            addApplicant = personDAO.getPerson(email);
+        } catch (DoesNotExistException ex) {
+
+            try {
+                personDAO.createApplicant(personDTO);
+            } catch (DuplicateEntryException | NullArgumentException ex1) {
+                Logger.getLogger(ApplicantResource.class.getName()).log(Level.SEVERE, null, ex1);
+            }
+
             value = Json.createObjectBuilder()
                     .add("added", "true")
-                    .add("username", user.getUsername())
-                    .add("password", user.getPassword())
                     .build();
-        } else {
+
+        } catch (NullArgumentException ex) {
             value = Json.createObjectBuilder()
-                    .add("added", "false")
+                    .add("added", "false, due to exception.")
                     .build();
+            Logger.getLogger(ApplicantResource.class.getName()).log(Level.SEVERE, null, ex);
         }
+
         return Response.status(200).entity(value).build();
     }
 
     @PUT
-    @Path("/updateuser")
-    @Consumes(MediaType.APPLICATION_JSON)
-    public Response updateAuth(User user) {
+    @Path("/update/{email}/{firstname}/{surname}/{ssn}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response updateApplicant(@PathParam("email") String email,
+            @PathParam("firstname") String firstname,
+            @PathParam("surname") String surname,
+            @PathParam("ssn") String ssn) {
         JsonObject value;
-// Get the existing user
-        User updateThisUser = em.find(User.class, user.getUsername());
-        if (updateThisUser != null) {
-            updateThisUser.setPassword(user.getPassword());
-            em.merge(updateThisUser);
+
+        PersonInterface updateApplicant = null;
+        try {
+            updateApplicant = personDAO.getPerson(email);
+
+        } catch (DoesNotExistException ex) {
+            Logger.getLogger(ApplicantResource.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (NullArgumentException ex) {
+            Logger.getLogger(ApplicantResource.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        if (updateApplicant != null) {
+            PersonDTO personDTO = new PersonDTO();
+
+            personDTO.setEmail(updateApplicant.getEmail());
+            personDTO.setFirstname(updateApplicant.getName());
+            personDTO.setSurname(updateApplicant.getSurname());
+            personDTO.setSsn(updateApplicant.getSsn());
+
+            try {
+                personDAO.updatePerson(personDTO);
+            } catch (NullArgumentException ex) {
+                Logger.getLogger(ApplicantResource.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (DoesNotExistException ex) {
+                Logger.getLogger(ApplicantResource.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
             value = Json.createObjectBuilder()
                     .add("updated", "true")
-                    .add("username", user.getUsername())
-                    .add("password", user.getPassword())
                     .build();
         } else {
             value = Json.createObjectBuilder()
@@ -125,27 +166,23 @@ public class ApplicantResource {
     }
 
     @DELETE
-    @Path("/deleteuser/{username}")
+    @Path("/delete/{email}")
     @Consumes(MediaType.APPLICATION_JSON)
-    public Response deleteAuth(@PathParam("username") String username) {
+    public Response deleteAuth(@PathParam("email") String email) {
         JsonObject value;
-// Get the existing user
-        User removeThisUser = em.find(User.class, username);
-        if (removeThisUser != null) {
-            em.remove(removeThisUser);
-            value = Json.createObjectBuilder()
-                    .add("deleted", "true")
-                    .add("username", username)
-                    .build();
-        } else {
-            value = Json.createObjectBuilder()
-                    .add("deleted", "false")
-                    .add("reason", "User dont exist")
-                    .build();
+        try {
+            personDAO.removePerson(email);
+        } catch (DoesNotExistException ex) {
+            Logger.getLogger(ApplicantResource.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (NullArgumentException ex) {
+            Logger.getLogger(ApplicantResource.class.getName()).log(Level.SEVERE, null, ex);
         }
+
+        value = Json.createObjectBuilder()
+                .add("deleted", "true")
+                .add("user", email)
+                .build();
         return Response.status(200).entity(value).build();
     }
 
-    
-    */
 }
